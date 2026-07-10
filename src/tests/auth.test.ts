5a1f969e87cd447dd370df42c5becdd00765ad63
FK78 <34196381+FK78@users.noreply.github.com>
2026-07-10 23:47:11 +0100

    Add Vitest for testing and create initial auth tests

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, test } from "vitest";
+
+
+describe("person", () => {
+  test("person is defined", () => {
+    expect(person).toBeDefined();
+  });
+
+  test("is active", () => {
+    expect(person.isActive).toBeTruthy();
+  });
+});
